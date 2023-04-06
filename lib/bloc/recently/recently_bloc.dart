bb5b14e060a72e8b4816d20064f660d031e14877
leywino <muhammadarshadv2@gmail.com>
2023-04-06 11:12:38 +0530

    recently bloc completed

--- lib/bloc/recently/recently_bloc.dart
+++ lib/bloc/recently/recently_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
 import 'package:firstproject/database/recently_played_model.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -8,9 +10,34 @@ part 'recently_state.dart';
 class RecentlyBloc extends Bloc<RecentlyEvent, RecentlyState> {
   RecentlyBloc() : super(const RecentlyInitial()) {
     on<GetAllRecently>((event, emit) {
-      final recentlybox = RecentlyBox.getInstance();
-      final recentlydb = recentlybox.values.toList();
-      emit(DisplayRecentlyState(recentlylist: recentlydb));
+      try {
+        final recentlybox = RecentlyBox.getInstance();
+        final recentlydb = recentlybox.values.toList();
+        emit(DisplayRecentlyState(recentlylist: recentlydb));
+      } catch (e) {
+        log("$e");
+      }
+    });
+
+    on<UpdateRecently>((event, emit) {
+      try {
+        final recentlybox = RecentlyBox.getInstance();
+        final list = recentlybox.values.toList();
+        bool isAlreadyAdded = list
+            .where((element) => element.songname == event.recsongs.songname)
+            .isEmpty;
+        if (isAlreadyAdded == true) {
+          recentlybox.add(event.recsongs);
+        } else {
+          int intindex = list.indexWhere(
+              (element) => element.songname == event.recsongs.songname);
+          recentlybox.deleteAt(intindex);
+          recentlybox.add(event.recsongs);
+        }
+        add(const GetAllRecently());
+      } catch (e) {
+        log("$e");
+      }
     });
   }
 }
